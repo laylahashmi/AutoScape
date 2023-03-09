389b25d23a50230627ba3276392dc181bfc2f1af
Kethlyn <africakp@outlook.com>
2023-03-09 13:21:57 -0400

    back-end completed

---
+++ sales/api/sales_rest/api_urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .api_views import api_list_salesperson, api_detail_salesperson, api_list_customer, api_detail_customer, api_list_salesrecord
+
+
+urlpatterns = [
+    path("salesperson/", api_list_salesperson, name="api_list_salesperson"),
+    path("salesperson/<int:employee_number>/", api_detail_salesperson, name="api_detail-salesperson"),
+    path("customer/", api_list_customer, name="api_list_customer"),
+    path("customer/<int:id>/", api_detail_customer, name="api_detail_customer"),
+    path("salesrecord/", api_list_salesrecord, name="api_list_salesrecord"),
+
+
+]
--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    vin=models.CharField(max_length=17, unique=True)
+    import_href=models.CharField(max_length=100, unique=True, null=True)
+
+
+class SalesPerson(models.Model):
+    name= models.CharField(max_length=100)
+    employee_number= models.IntegerField(unique=True, null=False)
+
+
+class Customer(models.Model):
+    name= models.CharField(max_length=100)
+    address= models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=16, unique=True)
+
+
+class SalesRecord(models.Model):
+    sale_price= models.CharField(max_length=14)
+
+    automobile= models.ForeignKey(
+       AutomobileVO,
+       related_name="automobile",
+        on_delete=models.CASCADE)
+
+    sales= models.ForeignKey(
+        SalesPerson,
+        related_name= "sales",
+        on_delete= models.CASCADE)
+
+    customer= models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.CASCADE)
